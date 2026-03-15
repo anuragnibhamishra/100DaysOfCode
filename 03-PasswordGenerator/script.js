365a357ac887ff5beca9556e7e1d511f1b059155
anuragnibhamishra <theanuragmishra19@gmail.com>
2026-03-15 20:26:20 +0530

    Day03

--- 03-PasswordGenerator/script.js
+++ 03-PasswordGenerator/script.js
@@ -5,20 +5,38 @@ const charGroups = {
     symbols: "!@#$%&*+-_"
 };
 const GenerateButton = document.querySelector("#generateButton");
-const renderDiv = document.querySelector("#renderDiv");
-function passwordGenerator() {
-    let finalCollection = charGroups.uppercases + charGroups.lowercases + charGroups.numbers + charGroups.symbols;
+const passwordDisplay = document.querySelector("#password");
+function passwordGenerator(length = 14) {
+    let finalCollection = "";
+    for (const group in charGroups) {
+        const checkbox = document.querySelector(`#${group}`);
+        if (checkbox.checked) {
+            finalCollection += charGroups[group];
+        }
+    }
 
-    const passwordLength = 12;
+    const passwordLength = length;
     let password = "";
-    for (let i = 0; i < passwordLength; i++) {
+    
+    if (finalCollection.length === 0) {
+        return "Please select at least one character type.";
+    } else {
+        for (let i = 0; i < passwordLength; i++) {
         const randomChar = finalCollection[Math.floor(Math.random() * finalCollection.length)];
         password += randomChar;
     }
+    }
+
     return password;
 }
 
 GenerateButton.addEventListener("click", () => {
-    let passwordGenerated = passwordGenerator()
-    renderDiv.textContent = passwordGenerated;
-})
+    const passwordGenerated = passwordGenerator();
+    passwordDisplay.textContent = passwordGenerated;
+});
+
+const copyBtn = document.querySelector("#copyBtn");
+copyBtn.addEventListener("click", () => {
+    const password = passwordDisplay.textContent;
+    navigator.clipboard.writeText(password);
+});
